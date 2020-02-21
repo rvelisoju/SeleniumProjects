fe8ae8acb31655e66fec6d54ed82e7054c9dce91
ranjith.velisoju@prolifics.com <ranjith.velisoju@prolifics.com>
2020-02-21 15:00:19 +0000

    First Commit of demo 2

---
+++ SampleDemo2/src/main/java/step_definitions/LoginStepDefinitions.java
@@ -0,0 +1,70 @@
+package step_definitions;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import junit.framework.Assert;
+
+public class LoginStepDefinitions {
+	
+	WebDriver driver;
+	
+		
+		 @Given("^user is already on Login Page$")
+		 public void user_already_on_login_page(){
+		 System.setProperty("webdriver.chrome.driver","E:\\Softwares\\chromedriver_win32\\chromedriver.exe");
+		 driver = new ChromeDriver();
+		 driver.get("http://thedemosite.co.uk/login.php");
+		 }
+	
+		 
+		 @When("^title of login page is Free CRM$")
+		 public void title_of_login_page_is_Free_CRM() throws Throwable {
+			 
+			 String title = driver.getTitle();
+			 System.out.println(title);
+			 Assert.assertEquals("Login example page to test the PHP MySQL online system", title);
+		    
+		 }
+
+		 @Then("^user enters username and password$")
+		 public void user_enters_username_and_password() throws Throwable {
+			 
+			 driver.findElement(By.name("username")).sendKeys("fdgghgfhgf");
+			 driver.findElement(By.name("password")).sendKeys("dfgdfg");
+		     
+		 }
+
+		 @Then("^user clicks on login button$")
+		 public void user_clicks_on_login_button() throws Throwable {
+			 
+			 WebElement loginBtn =
+					 driver.findElement(By.name("FormsButton2"));
+					 JavascriptExecutor js = (JavascriptExecutor)driver;
+					 js.executeScript("arguments[0].click();", loginBtn);
+		     
+		 }
+
+		 @Then("^user checks for success message$")
+		 public void user_checks_for_success_message() throws Throwable {
+			 
+		String str =	 driver.findElement(By.xpath("/html/body/table/tbody/tr/td[1]/big/blockquote/blockquote/font/center/b")).getText();
+		
+		Assert.assertEquals("**Failed Login**", str);
+
+		 }
+
+		 @Then("^close the browser$")
+		 public void close_the_browser() throws Throwable {
+			 driver.quit();
+		 }
+
+
+}
---
+++ SampleDemo2/test-outout/report.js
@@ -0,0 +1,95 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("C:/Users/rvelisoju/git/SeleniumProjects/SampleDemo1/FeatureFiles/Features/demo.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Free CRM Login Feature",
+  "description": "",
+  "id": "free-crm-login-feature",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "comments": [
+    {
+      "line": 4,
+      "value": "#without Examples Keyword"
+    }
+  ],
+  "line": 5,
+  "name": "Free CRM Login Test Scenario",
+  "description": "",
+  "id": "free-crm-login-feature;free-crm-login-test-scenario",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 7,
+  "name": "user is already on Login Page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 9,
+  "name": "title of login page is Free CRM",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 10,
+  "name": "user enters username and password",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 11,
+  "name": "user clicks on login button",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 12,
+  "name": "user checks for success message",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 13,
+  "name": "close the browser",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "LoginStepDefinitions.user_already_on_login_page()"
+});
+formatter.result({
+  "duration": 6857324500,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStepDefinitions.title_of_login_page_is_Free_CRM()"
+});
+formatter.result({
+  "duration": 12964800,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStepDefinitions.user_enters_username_and_password()"
+});
+formatter.result({
+  "duration": 225436400,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStepDefinitions.user_clicks_on_login_button()"
+});
+formatter.result({
+  "duration": 176451600,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStepDefinitions.user_checks_for_success_message()"
+});
+formatter.result({
+  "duration": 37740700,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStepDefinitions.close_the_browser()"
+});
+formatter.result({
+  "duration": 831751400,
+  "status": "passed"
+});
+});
